7a0ee1e80c7d09b0d83e3802146fb15041fc18dd
flavioleoneramos <flavioleone8383@gmail.com>
2022-05-17 20:18:44 -0300

    cadastroOk

--- _contr/verCadastro.php
+++ _contr/verCadastro.php
@@ -9,4 +9,29 @@ if($senha != $senhaR){
     header('location: ../cadastro.php');
 }
 
+require_once "../_class/conexaoClass.php";
+require_once "../_class/usuarioClass.php";
+
+$db = new conexaoClass();
+$user = new usuarioClass();
+
+$conn = new mysqli($db->getLocalhost(),$db->getUsuario(),$db->getSenha(),$db->getNomeBanco());
+
+if($conn->connect_error){
+    die('Erro ao conectar' . $conn->connect_error);
+}
+
+$user->setNome($nome);
+$user->setEmail($email);
+$user->setSenha($senha);
+$sql = "INSERT INTO usuario (nome,email,senha) VALUES ('$nome','$email','$senha')";
+
+if($conn->query($sql) === true){
+    echo "Dados gravados com sucesso";
+    header('location: ../perfil.php');
+}else{
+    echo "Error " . $sql . "</br>" . $conn->connect_error;
+}
+
+$conn->close();
 ?>
