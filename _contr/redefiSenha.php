94d9b15ed358dd75d7208e46e03f027642ec9bc2
flavioleoneramos <flavioleone8383@gmail.com>
2022-06-01 20:40:06 -0300

    recupsenha

---
+++ _contr/redefiSenha.php
@@ -0,0 +1,12 @@
+<?php
+$codigo = $_POST["idCodigo"];
+$novaSenhaR = $_POST["idNovaSenha"];
+
+echo $codigo; 
+
+echo $novaSenhaR;
+
+//verificar o código se é igual
+//Se sim dar um update e redireciona para perfil
+//Se não volta pra recupera senha
+?>
--- _contr/verRecupSenha.php
+++ _contr/verRecupSenha.php
@@ -11,6 +11,10 @@
 <script>
   alert("Informe o código recebido em seu email")
 </script>
-<?php
 
-?>
+<h1>Informe o código abaixo e redefina sua senha</h1>
+<form method="post" action="redefiSenha.php">
+  <label for="idCodigo">Código: </label><input type="text" name="idCodigo" id="idCodigo">
+  <label for="idNovaSenha">Nova senha: </label> <input type="password" name="idNovaSenha" id="idNovaSenha">
+  <input type="submit" value="Redefinir senha">
+</form>
